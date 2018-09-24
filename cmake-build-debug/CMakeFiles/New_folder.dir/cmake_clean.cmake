18d96e7d2a6cc1770c44257a7a22a4b02ac092b8
Le Bao Trung <trunglbth1809003@fpt.edu.vn>
2018-09-24 19:03:23 +0700

    Mo dau

---
+++ cmake-build-debug/CMakeFiles/New_folder.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/New_folder.dir/main.c.obj"
+  "New_folder.pdb"
+  "New_folder.exe"
+  "New_folder.exe.manifest"
+  "libNew_folder.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/New_folder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
